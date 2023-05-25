c6f3d210c4f861223328c8742ca7f1ca2d41beee
Miralles78 <josepmiralles@iesmontsia.org>
2023-05-25 23:27:20 +0200

    be

--- formulari2.php
+++ formulari2.php
@@ -35,7 +35,7 @@ $headers .= "Content-Type: text/plain; charset=UTF-8" . "\r\n";
 // send email
 mail($to,$subject,$msg,$headers);
 
-header('Location: Contacte.html');
+header('Location: index.html');
 echo <<<HTML
 <html>
 <body>
